cd71cf234843ce031c6aa840adaffb4aee681f4d
zbc <1511828529@qq.com>
2018-05-05 17:25:15 +0800

    Vue Test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 ﻿
 var controllerData = {
-	name: '',
+	name: 'Vue.js',
 	link: "<a href='http://www.baidu.com' target='blank'>Baidu</a>",
 	url:"http://www.sina.com",
 
@@ -8,7 +8,48 @@ var controllerData = {
 	text: "hehe",
 	num1: 10,
 	num2: 78,
-	num3: 987
+  num3: 987,
+
+  //v-bind data
+  cls1: true,
+  cls2: false,
+  cls3: false,
+
+  //
+  status: 1,
+
+  ifshow: false,
+  ifshowText:"点击以显示循环指令演示样本",
+  books: [
+    { name: "《Vue实战教学》", extraText: "zbc 著" },
+    { name: "《深入浅出React》", extraText: "zbc 著" },
+    { name: "《揭秘CSS3》", extraText: "zbc 著" }
+  ],
+
+  //template Data
+  templateObj: {
+    name: "zbc",
+    sex: "male",
+    age: "25"
+  },
+
+  //form Data
+  R1: false,
+
+  R2: 'r2',
+
+  R3_1: false,
+  R3_2: false,
+  R3_3: true,
+
+  selector: "js",
+
+  lazyMsg: "",
+  numberMsg: "",
+  trimMsg: "",
+
+
+
 
 };
 
@@ -36,7 +77,48 @@ var app = new Vue({
 		toNum: (x) => {
 			if (x === "") return 0;
 			return parseInt(x);
-		}
+    },
+    toggleBtnCls: function () {
+      if (this.cls1 === true) { this.cls1 = false; this.cls2 = true; this.cls3 = false; }
+      else if (this.cls2 === true) { this.cls1 = false; this.cls2 = false; this.cls3 = true; }
+      else if (this.cls3 === true) { this.cls1 = true; this.cls2 = false; this.cls3 = false; }
+      //console.log('你点击了一次切换按钮类名的按键');
+      //console.log(this.cls1, this.cls2, this.cls3);
+    },
+    toggleStatus: function () {
+      if (this.status === 1) { this.status = 2; }
+      else if (this.status === 2) { this.status = 3; }
+      else { this.status = 1; }
+      //console.log(this.status);
+    },
+    showFor: function () {
+      if (this.ifshow === true) { this.ifshow = false; this.ifshowText = "点击以显示循环指令演示样本" }
+      else { this.ifshow = true; this.ifshowText = "收起"};
+    },
+
+    clickFn2: function (x) {
+      console.log(x);
+      console.log(typeof x)
+    },
+
+    submit1: function () {
+      let f1 = document.getElementById('form1');
+      for (let i = 0; i < f1.children.length; i++) {
+        if (f1.children[i].nodeName === "INPUT")
+          console.log(f1.children[i].checked)
+      }
+      console.log(f1.value)
+    },
+
+    submit2: function () {
+      let f2 = document.getElementById('form2');
+      for (let i = 0; i < f2.children.length; i++) {
+        if (f2.children[i].nodeName === "INPUT")
+          console.log(f2.children[i].checked)
+      }
+      console.log(f2.value)
+    }
+
 	}
 
 
